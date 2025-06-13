3223ee3ab761114098b319207bd14cb4bde3d4fe
Samuel Gómez <samusmusic18@gmail.com>
2025-06-13 09:54:54 -0500

    Generador de numeros aleatorios, version final

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 int main(){
 	int Nm, Cn, Na;
-	cout<<Ingrese hasta que numero se generara los numeros aleatorios<<endl;
+	cout<<"Ingrese el numero maximo de la serie de numeros aleatorios"<<endl;
 	cin>>Nm;
-	cout<<Ingre la cantidad de numeros aleatorios<<endl;
+	cout<<"Ingrese la cantidad de numeros aleatorios"<<endl;
 	cin>>Cn;
 	if(Nm>=0){
 		if(Cn>0){
-			
+			for(int i=1; i<=Cn; i++){
+				Na= rand() % Nm +1;
+				cout<<Na<<endl;
+			}
 		}else{
-			cout<<Error: Numero invalido<<endl;
+			cout<<"Error: Numero invalido"<<endl;
 		}
 	}else{
-		cout<<Error: Numero negativo detectado<<endl;
+		cout<<"Error: Numero negativo detectado"<<endl;
 	}
-	
 	return 0;
 }
