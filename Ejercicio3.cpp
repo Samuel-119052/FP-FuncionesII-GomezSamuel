3e02e24d9caef8c1d3c7fdd8655d0508637c3a4e
Samuel Gómez <samusmusic18@gmail.com>
2025-06-13 11:11:17 -0500

    Avance 1: Validacion ante numeros negativos

---
+++ Ejercicio3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double C1, C2, H;
+	cout<<Ingre los valores de los catetos<<endl;
+	cin>>C1>>C2;
+	if(C1>0){
+		if(C2>0){
+			
+		}else{
+			cout<<Error numero negativo detectado<<endl;
+		}
+	}else{
+		cout<<Error: Numero negativo detectado<<endl;
+	}
+	return 0;
+}
