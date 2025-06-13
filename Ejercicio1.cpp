892f5ae663b7c57540a563746d29d28e2aa022e3
Samuel Gómez <samusmusic18@gmail.com>
2025-06-12 21:19:09 -0500

    Avance 1: Manejo de numeros negativos

---
+++ Ejercicio1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double r, Ac;
+	cout<<Ingrese el radio del circulo<<endl;
+	cin>>r;
+	if(r>=0){
+		
+	}else{
+		cout<<Error numero negativo detectado<<endl;
+	}
+	return 0;
+}
