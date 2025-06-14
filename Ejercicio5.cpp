89b6ed4f253f12f0a54b2e5c3cf13f216f344dfd
Samuel Gómez <samusmusic18@gmail.com>
2025-06-14 09:04:31 -0500

    Avance 1: Creando funcion void calcular raices

---
+++ Ejercicio5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void CalcularRaices(double a, double b, double c, double &X1, double &X2, bool &Sr){
+	double D;
+	D = pow(b, 2) - (4*a*c);
+	if(D>=0){
+		X1= (-b + sqrt(D))/(2*a);
+		X2= (-b - sqrt(D))/(2*a);
+		Sr=true;
+	}else{
+		X1=-b/(2*a);
+		X2= sqrt(-D)/(2*a);
+		Sr=false;
+	}
+}
+int main(){
+	double a, b, c, X1, X2;
+	bool R;
+	cout<<Ingrese los coeficientes de la ecuacion cuadratica<<endl;
+	cin>>a>>b>>c;
+	CalcularRaices(a, b, c, X1, X2, R);
+	return 0;
+}
