306b6ddfdca08d819251286c3cbd94e7f56d1236
Samuel Gómez <samusmusic18@gmail.com>
2025-06-13 20:28:26 -0500

    Calculo de hipotenusa, version final

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 int main(){
 	double C1, C2, H;
-	cout<<Ingre los valores de los catetos<<endl;
+	cout<<"Ingrese los valores de los catetos del triangulo rectangulo"<<endl;
 	cin>>C1>>C2;
 	if(C1>0){
 		if(C2>0){
-			
+			H= sqrt(pow(C1, 2) + pow(C2, 2));
+			cout<<"La hipotenusa del triangulo rectangulo es: "<<H<<endl;
 		}else{
-			cout<<Error numero negativo detectado<<endl;
+			cout<<"Error numero negativo detectado"<<endl;
 		}
 	}else{
-		cout<<Error: Numero negativo detectado<<endl;
+		cout<<"Error: Numero negativo detectado"<<endl;
 	}
 	return 0;
 }
