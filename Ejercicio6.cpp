2f0e2a35e788be5ce36b7fc9ed06fc80732715e0
Samuel Gómez <samusmusic18@gmail.com>
2025-06-14 11:27:02 -0500

    Avance 1: Agregando menu de piedra, papel o tijera

---
+++ Ejercicio6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+	int F, Pu, Pc, Ju, Jc;
+	F=0
+	while(F==0){
+		cout<<1. Piedra<<endl;
+		cout<<2. Papel<<endl;
+		cout<<3. Tijeras<<endl;
+		cout<<Elije una opcion<<endl;
+		cin>>Ju;	
+	}
+	return 0;
+}
